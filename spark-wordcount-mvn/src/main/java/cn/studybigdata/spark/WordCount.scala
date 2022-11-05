d880da1398e5236b0d1b092678411047e031e499
A-stranger <qingyuan.dev@Live.com>
2022-11-06 07:24:27 +0800

    many

---
+++ spark-wordcount-mvn/src/main/java/cn/studybigdata/spark/WordCount.scala
@@ -0,0 +1,18 @@
+package cn.studybigdata.spark
+
+import org.apache.spark.{SparkConf, SparkContext}
+
+/**
+ * file:///d:/bigdata.txt
+ */
+object WordCount {
+  def main(args: Array[String]): Unit = {
+
+    val conf = new SparkConf().setAppName("WordCountScala").setMaster("local");
+    val sc = new SparkContext(conf)
+
+    val result = sc.textFile("file:///d:/bigdata.txt").flatMap(_.split(" ")).map((_, 1)).reduceByKey(_ + _)
+    result.foreach(println)
+    sc.stop()
+  }
+}
